5c29f8374cd2b2798b8b0d9c86d56108d9291c3c
EllaPodvalny <ellapodvalny@gmail.com>
2020-06-23 16:44:35 -0400

    combine the two rest blocks for cross-corr analysis

--- 02_Analysis/Sensor_space/Sensor_pupil_cross_corr.py
+++ 02_Analysis/Sensor_space/Sensor_pupil_cross_corr.py
@@ -37,7 +37,7 @@ timelags = np.linspace(-1500, 1500, 31)
 lags = np.round(HLTP_pupil.resamp_fs * timelags / 1000).astype('int')
 
 for b in ['rest01', 'rest02', 'task_prestim']:
-    all_cc = []    
+    all_cc = {}
     for subject in subjects:
         cc = []
         filename = MEG_pro_dir + '/' + subject + '/' + b + '_ds-epo.fif'
@@ -50,59 +50,111 @@ for b in ['rest01', 'rest02', 'task_prestim']:
         for ep in range(len(epochs.events)):
             cc.append(meg_cross_corr_sampled(mdata[ep, :, :], 
                                              pdata[ep, :], lags))
-        all_cc.append(np.array(cc))
+        all_cc[subject] = [];
+        all_cc[subject] = cc.copy()
     HLTP_pupil.save(all_cc, HLTP_pupil.MEG_pro_dir + '/pupil_result' + 
                         '/cross_corr_' + b + '.pkl')
-# blp cross-corr
-for fband, freq in HLTP_pupil.freq_bands.items():      
-    for b in ['rest01', 'rest02']:
-        all_cc = []    
-        for subject in subjects:
-            cc = []
-            filename = MEG_pro_dir + '/' + subject + '/' + b + '_ds' \
-                + fband + '-epo.fif'
-            if not path.exists(filename): 
-                print('No such file'); continue
-            epochs = mne.read_epochs(filename)
-            pdata = epochs._data[:, HLTP_pupil.pupil_chan, :].copy()
-            epochs.pick_types(meg = True, ref_meg = False, exclude=[])
-            mdata = epochs._data
-            for ep in range(len(epochs.events)):
-                cc.append(meg_cross_corr_sampled(mdata[ep, :, :], 
-                                                 pdata[ep, :], lags))
-            all_cc.append(np.array(cc))
-        HLTP_pupil.save(all_cc, HLTP_pupil.MEG_pro_dir + '/pupil_result' + 
-                            '/cross_corr_' + b + fband + '.pkl')
 
-# group level analysis
-for b in ['rest01', 'rest02', 'task_prestim']:
-    for fband, freq in HLTP_pupil.freq_bands.items():      
-        # load array of correlation coefficient
-        all_cc = HLTP_pupil.load(HLTP_pupil.MEG_pro_dir + '/pupil_result' + 
-                            '/cross_corr_' + b + fband + '.pkl')
-        # fisher transform and average across trials
-        x = np.array([np.arctanh(np.array(all_cc[s])).mean(axis = 0) 
-                                                for s in range(len(all_cc))])
-        # t-test with spatio-temporal cluster correction for multiple comparisons
-        x = np.swapaxes(x, 1, 2)
-        connectivity, pos = HLTP_pupil.get_connectivity()   
-        alpha = 0.05; p_accept = 0.05
-        threshold = -distributions.t.ppf( alpha / 2.,  len(all_cc) - 1)
-        cluster_stats = mne.stats.spatio_temporal_cluster_1samp_test(x, 
-                                  n_permutations = 1000,
-                                  threshold = threshold, tail=0,
-                                  n_jobs = 1, connectivity=connectivity)
-        T_obs, clusters, p_values, _ = cluster_stats   
+# combine the two blocks of rest trials
+all_cc1= HLTP_pupil.load(HLTP_pupil.MEG_pro_dir + '/pupil_result' + 
+                        '/cross_corr_rest01.pkl')
+all_cc2= HLTP_pupil.load(HLTP_pupil.MEG_pro_dir + '/pupil_result' + 
+                        '/cross_corr_rest02.pkl')
+for subject in subjects:
+    all_cc1[subject] = np.concatenate(
+        [all_cc1[subject], all_cc2[subject]], axis = )
+HLTP_pupil.save(all_cc1, HLTP_pupil.MEG_pro_dir + '/pupil_result' + 
+                        '/cross_corr_rest.pkl')
+
+
+
+
+for b in ['rest', 'task_prestim']:
+    # load array of correlation coefficient
+     
+    all_cc = HLTP_pupil.load(HLTP_pupil.MEG_pro_dir + '/pupil_result' + 
+                        '/cross_corr_' + b + fband + '.pkl')
+    # fisher transform and average across trials
+    x = np.array([np.arctanh(np.array(all_cc[s])).mean(axis = 0) 
+                                            for s in range(len(all_cc))])
+    # t-test with spatio-temporal cluster correction for multiple comparisons
+    x = np.swapaxes(x, 1, 2)
+    connectivity, pos = HLTP_pupil.get_connectivity()   
+    alpha = 0.05; p_accept = 0.05
+    threshold = -distributions.t.ppf( alpha / 2.,  len(all_cc) - 1)
+    cluster_stats = mne.stats.spatio_temporal_cluster_1samp_test(x, 
+                              n_permutations = 1000,
+                              threshold = threshold, tail=0,
+                              n_jobs = 1, connectivity=connectivity)
+    T_obs, clusters, p_values, _ = cluster_stats   
+    
+    mask = np.zeros(T_obs.shape, dtype=bool)
+    good_cluster_inds = np.where( p_values < p_accept)[0]
+    if len(good_cluster_inds) > 0:
+        for g in good_cluster_inds:
+            sensors =  clusters[g][1]
+            times = clusters[g][0]        
+            mask[times, sensors.astype(int)] = True
+    HLTP_pupil.save(mask, HLTP_pupil.MEG_pro_dir + '/pupil_result' + 
+                        '/cross_corr_sig_mask_' + b + fband + '.pkl')
+
+
+
+
+
+
+
+
+# # blp cross-corr
+# for fband, freq in HLTP_pupil.freq_bands.items():      
+#     for b in ['rest01', 'rest02']:
+#         all_cc = []    
+#         for subject in subjects:
+#             cc = []
+#             filename = MEG_pro_dir + '/' + subject + '/' + b + '_ds' \
+#                 + fband + '-epo.fif'
+#             if not path.exists(filename): 
+#                 print('No such file'); continue
+#             epochs = mne.read_epochs(filename)
+#             pdata = epochs._data[:, HLTP_pupil.pupil_chan, :].copy()
+#             epochs.pick_types(meg = True, ref_meg = False, exclude=[])
+#             mdata = epochs._data
+#             for ep in range(len(epochs.events)):
+#                 cc.append(meg_cross_corr_sampled(mdata[ep, :, :], 
+#                                                  pdata[ep, :], lags))
+#             all_cc.append(np.array(cc))
+#         HLTP_pupil.save(all_cc, HLTP_pupil.MEG_pro_dir + '/pupil_result' + 
+#                             '/cross_corr_' + b + fband + '.pkl')
+
+# # group level analysis
+# for b in ['rest01', 'rest02', 'task_prestim']:
+#     for fband, freq in HLTP_pupil.freq_bands.items():      
+#         # load array of correlation coefficient
+#         all_cc = HLTP_pupil.load(HLTP_pupil.MEG_pro_dir + '/pupil_result' + 
+#                             '/cross_corr_' + b + fband + '.pkl')
+#         # fisher transform and average across trials
+#         x = np.array([np.arctanh(np.array(all_cc[s])).mean(axis = 0) 
+#                                                 for s in range(len(all_cc))])
+#         # t-test with spatio-temporal cluster correction for multiple comparisons
+#         x = np.swapaxes(x, 1, 2)
+#         connectivity, pos = HLTP_pupil.get_connectivity()   
+#         alpha = 0.05; p_accept = 0.05
+#         threshold = -distributions.t.ppf( alpha / 2.,  len(all_cc) - 1)
+#         cluster_stats = mne.stats.spatio_temporal_cluster_1samp_test(x, 
+#                                   n_permutations = 1000,
+#                                   threshold = threshold, tail=0,
+#                                   n_jobs = 1, connectivity=connectivity)
+#         T_obs, clusters, p_values, _ = cluster_stats   
         
-        mask = np.zeros(T_obs.shape, dtype=bool)
-        good_cluster_inds = np.where( p_values < p_accept)[0]
-        if len(good_cluster_inds) > 0:
-            for g in good_cluster_inds:
-                sensors =  clusters[g][1]
-                times = clusters[g][0]        
-                mask[times, sensors.astype(int)] = True
-        HLTP_pupil.save(mask, HLTP_pupil.MEG_pro_dir + '/pupil_result' + 
-                            '/cross_corr_sig_mask_' + b + fband + '.pkl')
+#         mask = np.zeros(T_obs.shape, dtype=bool)
+#         good_cluster_inds = np.where( p_values < p_accept)[0]
+#         if len(good_cluster_inds) > 0:
+#             for g in good_cluster_inds:
+#                 sensors =  clusters[g][1]
+#                 times = clusters[g][0]        
+#                 mask[times, sensors.astype(int)] = True
+#         HLTP_pupil.save(mask, HLTP_pupil.MEG_pro_dir + '/pupil_result' + 
+#                             '/cross_corr_sig_mask_' + b + fband + '.pkl')
 
 
     
